6238f6f18461c478bc8794717d72637166b43be4
DaveGC97 <88519545+DaveGC97@users.noreply.github.com>
2024-02-14 21:47:55 -0600

    cache

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,31 @@
 if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js')
 }
+if (window.caches) {
+    //console.log('soporta cache')
+    //Crear el primer cache
+    caches.open('test-1')
+    caches.open('test-2')
+
+    //Verificar si existe un cache
+    caches.has('test-1').then(console.log)
+
+    //Eliminar un cache
+    caches.delete('test-1').then(console.log)
+
+    caches.open('cache-v1').then(cache => {
+        //Agregar archivos
+        cache.add('/index.html')
+        //Agregar varios archivos
+        cache.addAll([
+            'logo192.png',
+            'logo512.png',
+            '/pages/offline.html'
+        ]).then(() => {
+            //Interceptar un archivo por otro
+            cache.put('index.html'), new Response('Interceptando archivo in')
+        })
+        //Obtener todos los caches
+        caches.keys().then(console.log)
+    })
+}
---
+++ public/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('fetch', event => {
+
+    const offlineResponse = fetch('pages/offline.html')
+
+    const resp = fetch(event.request)
+        .catch(() => offlineResponse)
+
+    event.respondWith(resp)
+})
